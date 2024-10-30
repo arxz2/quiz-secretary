3d810742418bd33d76fc10cca86160058f43be3a
Никита Попов <n.popov@automacon.ru>
2024-10-30 22:31:12 +0100

    polls and games info

--- checker.py
+++ checker.py
@@ -1,4 +1,5 @@
-import os
+import json
+import http.client
 import requests
 import argparse
 from bs4 import BeautifulSoup
@@ -12,34 +13,77 @@ args = parser.parse_args()
 data_file = "games.txt"
 
 # Change URL for a different city or game types
-url = "https://beg.quizplease.ru/schedule"
+url = "https://beg.quizplease.ru/"
 
-r = requests.get(url)
+r = requests.get(url + "schedule")
 print(f"Fetching games data: HTTP {r.status_code}")
 
 soup = BeautifulSoup(r.text, "html.parser")
-data = soup.css.select(".schedule-block-head.w-inline-block")
+games = soup.select("div.schedule-column")
 
-games = list(
-    map(
-        lambda x: x.attrs["href"].rsplit("=")[-1],
-        data,
-    )
-)
+def send_msg(token, chat_id, msg):
+        r = requests.post(
+            url=f"https://api.telegram.org/bot{token}/sendMessage",
+            data={"chat_id": chat_id, "text": msg},
+        )
+
+def send_poll(token, chat_id, question, options):
+    # Формируем данные для отправки
+    poll_data = {
+        "chat_id": chat_id,
+        "question": question,
+        "options": options,
+        "is_anonymous": False
+    }
+
+    # Создаем JSON-строку из данных
+    poll_json = json.dumps(poll_data)
+
+    # Подготавливаем HTTP-запрос
+    conn = http.client.HTTPSConnection("api.telegram.org")
+    url = f"/bot{token}/sendPoll"
+    headers = {'Content-type': 'application/json'}
+
+    # Отправляем запрос
+    conn.request("POST", url, poll_json, headers)
+
+    # Получаем ответ
+    response = conn.getresponse()
+    data = response.read()
+
+    # Печатаем ответ
+    #print(data.decode("utf-8"))
+
+    # Закрываем соединение
+    conn.close()
 
 filename = Path(data_file)
 filename.touch(exist_ok=True)
 with open(filename, "r+") as f:
     lines = f.read().splitlines()
     for game in games:
-        if game in lines:
+        game_id = game.get("id")
+        game_date = game.select_one(".block-date-with-language-game").get_text(strip=True)
+        game_name = game.select_one(".schedule-block-head .h2-game-card.h2-left").get_text(strip=True) 
+        game_number = game.select_one(".schedule-block-head .h2-game-card:not(.h2-left)").get_text(strip=True)
+        game_location = game.select_one(".schedule-block-info-bar").get_text(strip=True)
+
+        if game_id in lines:
             continue
-        msg = f"ЗАПИШИСЬ БЛЯТЬ НА ИГРУ!\nhttps://quizplease.ru/game-page?id={game}"
-        print(f"Game found: https://quizplease.ru/game-page?id={game}")
+
+        if "Четверг" in game_date:
+            msg = f"Запишись на игру!\n{game_date}, {game_name} {game_number}\n{url}/game-page?id={game_id}"
+            send_msg(args.token, args.chatId, msg)
+            
+        else:
+            question = f"{game_name} {game_number}\n{game_date}, {game_location}"
+            msg = f"{url}/game-page?id={game_id}"
+
+            options = ["Иду", "Иду +1", "Не иду", "Посмотреть ответы"]
+            send_poll(args.token, args.chatId, question, options)
+            send_msg(args.token, args.chatId, msg)
+
+        print(msg)
         
-        r = requests.post(
-            url=f"https://api.telegram.org/bot{args.token}/sendMessage",
-            data={"chat_id": args.chatId, "text": msg},
-        )
         print(f"Sending message: HTTP {r.status_code}")
-        f.write(game + "\n")
+        f.write(game_id + "\n")
